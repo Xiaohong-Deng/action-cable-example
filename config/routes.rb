633bc2067939c6ffc7aed8d69c9474e6e2f966f8
nicenick <worldsky86rough@gmail.com>
2017-08-26 19:26:01 +0800

    Add all actioncable logic

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "chatrooms#index"
+  mount ActionCable.server, at: '/cable'
+  resources :chatrooms, param: :slug
+  resources :messages
 end
